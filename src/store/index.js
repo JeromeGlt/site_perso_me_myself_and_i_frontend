e38f4d5da3f62a2e4533a642e50fce096f88f5e9
JeromeGlt <jerome.gallot39@orange.fr>
2023-03-07 19:14:38 +0100

    creation of a form to sign up, which call a function to fetch with the bakend

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,37 @@ export default createStore({
   getters: {
   },
   mutations: {
+    SIGNUP (state, data) {
+      state.username = data.username,
+      state.userId = data.userId,
+      state.token = data.token,
+      state.message = data.message
+    }
   },
   actions: {
+
+    submitSignup({ commit }, values) {
+      console.log(JSON.stringify(values))
+      fetch('http://localhost:3000/api/user/signup', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      })
+      .then(res => res.json())
+      .then(data => {
+        commit('SIGNUP', data)
+        // if(!data.message) {
+          // router.push('/posts')
+        // }
+        console.log(data)
+        localStorage.setItem('token', data.token)
+      })
+      .catch(err => console.log(err))
+  }
+
   },
   modules: {
   }
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -3,17 +3,17 @@
     <div>
       <p>Remplissez les deux champs afin de créer votre compte</p>
       <div>
-        <label for='pseudo'>Pseudo</label>
-        <input type="text" name="pseudo" v-model="pseudo" @change="pseudoInput">
-        <div v-if="alertPseudo">Le pseudo doit contenir entre 5 et 40 caractères. Seuls les lettres, points et tirets sont autorisés</div>
+        <label for='username'>Pseudo</label>
+        <input type="text" name="username" v-model="username" @input="usernameInput">
+        <div v-if="alertUsername">Le pseudo doit contenir entre 5 et 40 caractères. Seuls les lettres, points et tirets sont autorisés</div>
       </div>
       <div>
         <label for='password'>Mot de passe</label>
-        <input type="password" name="password" v-model="password" @change="passwordInput">
+        <input type="password" name="password" v-model="password" @input="passwordInput">
         <div v-if="alertPassword">Le mot de passe doit contenir entre 8 et 100 caractères dont une minuscule, une majuscule et un chiffre</div>
       </div>
-      <p v-if="!this.pseudo || !this.password ||alertPseudo || alertPassword">Veuillez remplir les deux champs</p>
-      <button v-if="this.pseudo && this.password && !alertPseudo && !alertPassword" @submit="click">Créer mon compte</button>
+      <p v-if="!this.username || !this.password || alertUsername || alertPassword">Veuillez remplir les deux champs</p>
+      <button v-if="this.username && this.password && !alertUsername && !alertPassword" @click="submit">Créer mon compte</button>
       <p >Si vous avez déjà un compte, <router-link to="/login">cliquez ici !</router-link></p>
     </div>
   </div>
@@ -24,24 +24,23 @@
 
   export default {
     name: 'SignUp',
-  //     computed: {
+    computed: {
   //         ...mapState({
   //             message: state => state.message
   //         }),
-  //     },
-  //     data: () => ({
-  //             pseudo: '',
-  //             password: '',
-  //             alertEmail: false,
-  //             alertPseudo: false,
-  //             alertPassword: false
-  //     }),
+    },
+    data: () => ({
+      username: '',
+      password: '',
+      alertUsername: false,
+      alertPassword: false
+    }),
     methods: {
-      pseudoInput() {
-        if(/[^a-zA-Z-_.]/i.test(this.pseudo)) {
-          return this.alertPseudo = true
+      usernameInput() {
+        if(/[^a-zA-Z-_.]/i.test(this.username) || this.username.length < 5 || this.username.length > 40) {
+          return this.alertUsername = true
         } else {
-          this.alertPseudo = false
+          this.alertUsername = false
         }
       },
       passwordInput() {
@@ -53,13 +52,13 @@
       },
       submit() {
 
-  //             let formData = new FormData()
-  //             formData.append('pseudo', this.pseudo)
-  //             formData.append('password', this.password)
-  //             formData.append('imageUrl', this.imageUrl)
-  //             formData.append('email', this.email)
+        let values = {
+          username: this.username,
+          password: this.password
+        }
+        console.log(values)
 
-        // this.$store.dispatch('submitSignup', formData)
+        this.$store.dispatch('submitSignup', values)
       }
     }
   }
